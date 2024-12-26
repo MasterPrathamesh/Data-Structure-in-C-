d2c3e30771f4a0e1c93eff553a2912cf5097d5c0
Prathamesh Deshmukh <109153108+MasterPrathamesh@users.noreply.github.com>
2024-12-26 22:03:52 +0530

    Add files via upload

---
+++ if-else.cpp
@@ -0,0 +1,15 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   char ch;
+//   cout << "Enter a character: ";
+//   cin >> ch;
+//   if (ch >= 'a' && ch <= 'z') {
+//     cout << "It is a lowercase character" << endl;
+//   } else if (ch >= 'A' && ch <= 'Z') {
+//     cout << "It is a uppercase character" << endl;
+//   } else if (ch >= '1' && ch <= '9') {
+//     cout << "It is a numeric character" << endl;
+//   }
+// }
