4c7f05afa7e9197af4b8cb6505e2c71a9a5a2286
Prathamesh Deshmukh <109153108+MasterPrathamesh@users.noreply.github.com>
2024-12-27 13:55:44 +0530

    Add files via upload
    Understanding While loop.

---
+++ while_loop.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//  int n;
+//  cin >> n;
+
+// int i = 1;
+// while (i <= n) {
+//   cout << i << " ";
+//   i = i + 1;
+// }
+/*
+  Finding the sum of the integers from 1 to n
+==================================================
+  int n;
+  cout << "Enter the value of n: ";
+  cin >> n;
+  int i = 1;
+  int sum = 0;
+  while (i <= n) {
+    sum = sum + i;
+    i = i + 1;
+  }
+  cout << "The sum of the numbers is : " << sum << endl; */
+
+// Finding the sum of the even numbers from 1 to n
+/*int n;
+cout << "Enter the value of n : " << endl;
+cin >> n;
+int i = 2;
+int sum = 0;
+while (i <= n) {
+  sum = sum + i;
+  i = i + 2;
+}
+cout << "The sum of the even numbers is : " << sum << endl;
+*/
+
+// Finding the prime numbers from 1 to n
+/* int n;
+ cout << " Enter the value of n : " << endl;
+ cin >> n;
+ int i = 2;
+ while (i < n) {
+   if (n % i == 0) {
+     cout << "Not a prime number for" << i << endl;
+   } else {
+     cout << "It is a prime number "  << i << endl;
+   }
+   i = i + 1;
+ }
+*/
+
+//}
