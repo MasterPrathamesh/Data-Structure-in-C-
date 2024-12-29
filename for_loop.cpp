40c1d5855a94d9cecc40104c00cecb347c634cfc
Prathamesh Deshmukh <109153108+MasterPrathamesh@users.noreply.github.com>
2024-12-29 11:19:26 +0530

    Add files via upload
    Understanding for loop.

---
+++ for_loop.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std ; 
+
+int main(){
+    // Printing the fibonacci series . 
+    // int n ; 
+    // cout << "Enter the value of N : " << endl; 
+    // cin >> n ; 
+    // int a = 0;
+    // int b = 1; 
+    // cout << a << " " << b  << " ";
+    // for(int i = 1 ; i <= n ; i++ ){
+    //     int sum = a + b ; 
+    //     cout << sum << " ";
+
+    //     a = b ; 
+    //     b = sum ; 
+    // }
+
+  // printing whether the no. is prime or not . 
+  // int n ; 
+  // cout << "Enter the value of n : " << endl; 
+  // cin >> n ; 
+
+  // bool isPrime = 1 ; 
+
+  // for(int i = 2 ; i < n ; i++){
+  //   if(n%i==0){
+  //     isPrime = 0;
+  //     // cout << "Not a Prime number for : " << i << endl ;
+  //     break;
+  //   }
+    
+  // }
+
+  // if(isPrime == 0){
+  //   cout << "Not a Prime Number  " << endl ; 
+  // }
+  // else{
+  //   cout << "is a Prime number  " << endl; 
+  // }
+
+// understanding the continue keyword . 
+
+  for(int i = 1 ; i<=5 ; i++){
+    cout << "Hiii !!" << endl; 
+    cout << "hey !! " << endl ; 
+    continue;
+    cout << "Reply toh karde !! " << endl ; 
+  }
+
+
+
+
+
+  
+}
