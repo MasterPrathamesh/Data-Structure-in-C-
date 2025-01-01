1641d8417c331b9e3eb4bca294f70a78ecdd23f5
Prathamesh Deshmukh <109153108+MasterPrathamesh@users.noreply.github.com>
2025-01-01 13:02:34 +0530

    Add files via upload
    Understanding Switch Case STatement.

---
+++ Lecture8.cpp
@@ -0,0 +1,81 @@
+// understanding the switch case statement :
+//  switch case statement is used to perform different actions based on different conditions.
+// #include<iostream>
+// using namespace std ; 
+
+// int main(){
+
+    // // using switch case : 
+    // int a , b ; 
+    // cout << "Enter the value of a : " << endl ; 
+    // cin >> a ; 
+    // cout << "Enter the value of b :" << endl ;
+    // cin >> b ; 
+
+    // char op ; 
+    // cout << "Enter the operator : " << endl ; 
+    // cin >> op ; 
+
+    // switch (op) {
+        
+    //     case '+' : cout << "The sum of a and b is : " << a+b << endl ;
+    //     break ;
+
+    //     case '-' : cout << "The difference of a and b is : " << a-b << endl ;
+    //     break ;
+
+    //     case '*' : cout << "The product of a and b is : " << a*b << endl ;
+    //     break ;
+
+    //     case '/' : cout << "The quotient of a and b is : " << a/b << endl ;
+    //     break ;
+
+
+    //     default :
+    //     cout << "Please enter a valid operator" << endl ;
+
+
+    // }
+
+
+// int power(int a , int b){
+//     int ans = 1 ; 
+//     for(int i = 1 ; i <= b ; i++){
+//         ans = ans * a ; 
+//     }
+//     return ans ; 
+// }
+
+
+// {
+//     int a,b ; 
+//     cout << "Enter the value of a : " << endl ; 
+//     cin >> a ; 
+//     cout << "Enter the value of b : " << endl ; 
+//     cin >> b ;
+    
+//     int ans = power(a,b); 
+//     cout << "The answer is : " << ans << endl ; 
+
+//     return 0 ; 
+
+
+
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
